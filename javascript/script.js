cf1c5d43d305ae4f9ec8e9da69d11f486ca426a0
SBanacka <scyrusb@yahoo.com>
2024-09-06 09:06:32 -0400

    Updated script and css to adjust font sizing and colors.

--- javascript/script.js
+++ javascript/script.js
@@ -7,9 +7,11 @@ navLinks.forEach((link) => {
 
     // Get the target ID from the link's href attribute
     const targetId = link.getAttribute("href").substring(1);
-    // Show all sections initially (optional, if you want to show all sections by default)
+    // Show all sections initially.
     sections.forEach((section) => section.classList.remove("hidden"));
-
+    //Adjust font color to display current page.
+    navLinks.forEach((eachLink) => eachLink.classList.remove("visible"));
+    link.classList.add("visible");
     // Hide all other sections except the target
     sections.forEach((section) => {
       if (section.id !== targetId) {
